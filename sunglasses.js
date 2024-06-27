d868aa3ad9d3dc06a6c74c89e2d99e63c9186825
Paul Stephen Bayot <110324874+Sinichi78@users.noreply.github.com>
2024-06-27 16:46:39 +0800

    first commit

---
+++ sunglasses.js
@@ -0,0 +1,18 @@
+ export const sunglasses = {
+    model: {
+        name: "aviator",
+        price: 300,
+        thumbImg: "./images/thumb-aviator.png",
+        cssClass: "frame-aviator",
+    },
+    lenses: {
+        color: "sepia",
+        price: 20,
+        cssClass: "color-sepia",
+    },
+    frame: {
+        color: "charcoal",
+        price: 0,
+        cssClass: "color-charcoal",
+    }     
+}
